6fcc1ba474182cd84e00b20fabc46e1e60e19399
Shadowx5671 <107345738+shadowx5671@users.noreply.github.com>
2025-03-20 10:39:56 +0100

    Update Unittest

---
+++ app/src/test/java/com/shadow/camerasensor/UnitTest.kt
@@ -0,0 +1,14 @@
+package com.shadow.camerasensor
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class UnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+
+}
